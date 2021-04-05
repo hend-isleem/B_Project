84933f40fd17a48ac5170ba58f21aba39bc2c962
hend_isleem <hendisleem@gmail.com>
2021-04-06 00:07:17 +0300

    deploy

--- backEnd/server.js
+++ backEnd/server.js
@@ -49,9 +49,9 @@ require("./app/routes/student.routes")(app);
 
 //Production Environment settings
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.resolve(__dirname, "..", "public")));
+  app.use(express.static(path.resolve(__dirname, "..", "build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "..", "public", "index.html"));
+    res.sendFile(path.resolve(__dirname, "..", "build", "index.html"));
   });
 }
 // set port, listen for requests
