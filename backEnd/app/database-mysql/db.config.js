d6556793da8f9c9acb07f2e74bcc2fce16e7bb42
hend_isleem <hendisleem@gmail.com>
2021-05-08 18:14:26 +0300

    working locally

--- backEnd/app/database-mysql/db.config.js
+++ backEnd/app/database-mysql/db.config.js
@@ -1,3 +1,24 @@
+/*
+DB name : osama_nahed
+User name : osama_nahed
+Passworrd ^z@yjcPE!b2b
+
+
+module.exports = {
+  HOST: "localhost",
+  USER: "osama_nahed",
+  PASSWORD: "^z@yjcPE!b2b",
+  DB: "osama_nahed",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+};
+
+*/
 module.exports = {
   HOST: "localhost",
   USER: "root",
